41429f69234042c939f2bdf25de82ad573491e4a
Rithik Bhandari <rithikbhandari895@gmail.com>
2022-05-12 01:47:08 +0530

    feat: added files

--- lib/local_storage_flutter.dart
+++ lib/local_storage_flutter.dart
@@ -1,7 +1,3 @@
-library local_storage_flutter;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+// @github/rithik-dev/
+//
+export 'src/local_storage.dart';
